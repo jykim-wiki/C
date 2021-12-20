9031b8f83a081e91e36e616a54e8b4e2dc29ea06
jykim-wiki <96406350+jykim-wiki@users.noreply.github.com>
2021-12-20 14:52:31 +0900

    Add files via upload

---
+++ 003/example01.c
@@ -0,0 +1,26 @@
+/*
+
+   The 1st exercise
+
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	short a;
+	long b;
+	int c;
+	float f;
+	double g;
+	
+	printf("size of short = %d\n",sizeof(a));
+	printf("size of long = %d\n",sizeof(b));
+	printf("size of int = %d\n",sizeof(c));
+	printf("size of float = %d\n",sizeof(f));
+	printf("size of double = %d\n",sizeof(g));
+	
+	return 0;
+}
+
---
+++ 003/example02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  float f1;
+  double f2;
+  printf("Enter number f1 & f2\n");
+  scanf("%f %lf",&f1,&f2);
+  printf("f1 = %f, f2 = %f\n",f1,f2);
+  return 0;
+}
